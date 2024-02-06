ab1998b9a411e11604e0a87325ca45dd245630e7
htlawson <htlawson@terpmail.umd.edu>
2024-02-06 16:52:19 -0500

    x

--- pre_lab_03/LAWSON_TEST.R
+++ pre_lab_03/LAWSON_TEST.R
@@ -1,1 +1,10 @@
-Test file from Wells
+#Test file from Wells
+
+install.packages("tidyverse")
+library(tidyverse)
+
+
+# Loading and Analyzing Data {#r-load-analyze-visualize}
+
+MediaBucks <- rio::import("https://docs.google.com/spreadsheets/d/1jkbQFwIdaWv8K00Ad6Wq7ZxFTUPFQA-g/edit#gid=1655992481", which = "RealMediaSalaries2")
+
